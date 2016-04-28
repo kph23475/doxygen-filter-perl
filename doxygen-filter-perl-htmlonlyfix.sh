4df944f232a4f13bb670a57c9c189a1e3552fee6
Kevin Hatfield <kevin.hatfield@gmail.com>
2016-04-28 16:39:19 -0400

    Add script for optional removal *<br> from htmlonly blocks

---
+++ doxygen-filter-perl-htmlonlyfix.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#
+# 2016-04-28
+#
+# @htmlonly doxygen comment markup introduced by
+# doxygen-filter-perl.
+#
+# `# @htmlonly` usage results in `* <br>` placement upon
+# each line
+#
+# remove unwanted leading ' ', '*', '<br>' from @htmlonly
+# blocks
+#
+# Example @htmlonly block Perl source input
+#
+#   #** @date 2016.04.25
+#   #*
+#   #** @copyright  Copyright (C) 2016, An author. All Rights Reserved.
+#   #*
+#   #** @see SomePackage
+#   #*
+#
+#   #** @class MyRecord
+#   #*
+#   #** @brief Storage for database View query information.
+#   #*
+#   #** @details Record layout or storage for desired entries in database containing desired information. 
+#   # Each instance variable name equals name of a View within database, with each View containing a single 
+#   # record.
+#   #
+#   #** @htmlonly 
+#   # <p>All instance variables: 
+#   # <ul>                    
+#   # <li>Defined in configuration apart from source. <a href="http:#search.cpan.org/perldoc/Config::Tiny">Config::Tiny</a> 
+#   # <li>Represent information from database for ease of conversion into JSON, provison of indirection for database layout. 
+#   # </ul> 
+#   # <table border=1>
+#   # <tr><td> Hash </td> 
+#   #     <td> Data </td> 
+#   #     <td> Note </td></tr>
+#   # <tr><td> <code>$this->{last_update} </code>                   </td> 
+#   #     <td> '2016.04.02 13:44:51'                                </td> 
+#   #     <td> status information from database View `last_update`  </td></tr>
+#   # <tr><td> <code>$this->{sensor01}{status}</code>        </td> 
+#   #     <td> 'OK' or 'Alert'                               </td> 
+#   # <td> from View `sensor01`                              </td></tr>
+#   # <tr><td> <code>$this->{sensor01}{moment}</code>        </td> 
+#   #     <td colpsan=2>  '2016.04.02 13:45:03'              </td></tr>
+#   # </table>
+#   # <div>Example:<p>
+#   # <code>my $MyRecord = SomePackage::MyRecord->new();<p>
+#   # $statusRecord->{'sensor01'}{'description'}="This sensor";<p>
+#   # print $statusRecord->{sensor01}{'description'};<p>
+#   # &gt;This Sensor</code></div>
+#   #** @endhtmlonly
+#
+./bin/doxygen-filter-perl $1                     \
+| /usr/bin/awk       'function removeLineBreak(lineIn)
+                      {
+                         gsub(/^[\t* ]{0,}<br>/,"",lineIn);
+                         print lineIn;
+                      }
+                      BEGIN{ htmltag=0;}
+                      {
+                      if ($0 ~ /@htmlonly/)
+                      {
+                         htmltagfound=1;
+                         removeLineBreak($0);
+                      }
+                      else if (htmltagfound==1)
+                      {
+                         removeLineBreak($0);
+                      }
+                      else if ($0 ~ /@endhtmlonly/)
+                      {
+                         htmltagfound=0;
+                         removeLineBreak($0);
+                      }
+                      else
+                      {
+                         print $0;
+                      }
+                      }';
